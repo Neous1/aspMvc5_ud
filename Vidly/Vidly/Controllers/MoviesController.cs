57aa3cca64bd310e224c7b6b6407936bd9532202
Neous1 <Neous1@users.noreply.github.com>
2017-12-14 16:38:52 -0500

    apply the route attribe to the controller

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -19,11 +19,13 @@ namespace Vidly.Controllers
             return View(movie);
         }
 
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+
         public ActionResult ByReleaseDate(int year , int month)
         {
             return Content(year +"/"+ month);
         }
-
+ /*
         public ActionResult Edit(int id)
         {
             return Content("id = " + id);
@@ -42,6 +44,6 @@ namespace Vidly.Controllers
                 sortBy = "Name";
 
             return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
+        }*/
     }
 }
