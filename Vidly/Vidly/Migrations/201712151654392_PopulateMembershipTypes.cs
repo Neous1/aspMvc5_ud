f97bd3f6e09b7aaea080a3be76fed310294ca79d
Neous1 <Neous1@users.noreply.github.com>
2017-12-15 12:09:57 -0500

    override name in customer in code

---
+++ Vidly/Vidly/Migrations/201712151654392_PopulateMembershipTypes.cs
@@ -0,0 +1,36 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMembershipTypes : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes");
+            DropIndex("dbo.Customers", new[] { "MembershipTypeId" });
+            DropPrimaryKey("dbo.MembershipTypes");
+            AddColumn("dbo.Customers", "MembershipType_Id", c => c.Int());
+            AddColumn("dbo.MembershipTypes", "DurationInMonth", c => c.Byte(nullable: false));
+            AlterColumn("dbo.MembershipTypes", "Id", c => c.Int(nullable: false, identity: true));
+            AddPrimaryKey("dbo.MembershipTypes", "Id");
+            CreateIndex("dbo.Customers", "MembershipType_Id");
+            AddForeignKey("dbo.Customers", "MembershipType_Id", "dbo.MembershipTypes", "Id");
+            DropColumn("dbo.MembershipTypes", "DurantionInMonths");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.MembershipTypes", "DurantionInMonths", c => c.Byte(nullable: false));
+            DropForeignKey("dbo.Customers", "MembershipType_Id", "dbo.MembershipTypes");
+            DropIndex("dbo.Customers", new[] { "MembershipType_Id" });
+            DropPrimaryKey("dbo.MembershipTypes");
+            AlterColumn("dbo.MembershipTypes", "Id", c => c.Byte(nullable: false));
+            DropColumn("dbo.MembershipTypes", "DurationInMonth");
+            DropColumn("dbo.Customers", "MembershipType_Id");
+            AddPrimaryKey("dbo.MembershipTypes", "Id");
+            CreateIndex("dbo.Customers", "MembershipTypeId");
+            AddForeignKey("dbo.Customers", "MembershipTypeId", "dbo.MembershipTypes", "Id", cascadeDelete: true);
+        }
+    }
+}
