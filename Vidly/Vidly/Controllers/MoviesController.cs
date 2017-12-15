e39c73d564aa787054a9e69613facce6507ff3c6
Neous1 <Neous1@users.noreply.github.com>
2017-12-15 00:08:47 -0500

    completed exercises 2

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -12,6 +12,22 @@ namespace Vidly.Controllers
     [System.Runtime.InteropServices.Guid("29F251BD-7674-4416-9D6C-8CCDEEDA64EC")]
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek"},
+                new Movie { Id = 2, Name = "Wall-e"}
+                
+            };
+        }
+
         // GET: Movies/Random 
         public ActionResult Random()
         {
